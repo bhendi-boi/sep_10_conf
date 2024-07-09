5a661609aeba29d558ff2d4456476e5676666625
bhendi-boi <bhendiisgreen@gmail.com>
2024-07-09 20:23:51 +0530

    removed a typo in fig3 title

--- graphs.m
+++ graphs.m
@@ -1,12 +1,12 @@
 clear; close all
-p = ones(1,4) * .25;
+p = ones(1, 4) * .25;
 
 % subplot(2,2,1)
 bar(p, "yellow");
-set(gca,'XTick',[])
+set(gca, 'XTick', [])
 xlabel("States");
 ylabel("Probability amplitude")
-ylim([-1,1]); xlim([0,5])
+ylim([-1, 1]); xlim([0, 5])
 title("Two qubits in a state of superposition")
 text(1, -0.1, '00');
 text(2, -0.1, '01');
@@ -14,22 +14,21 @@ text(3, -0.1, '10');
 text(4, -0.1, '11');
 
 hold on
-p = ones(1,6) * .25;
-plot(0:5,p, Color="r", LineStyle="--", LineWidth=1)
-
+p = ones(1, 6) * .25;
+plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
 
 %%
 
 clear;
-p = ones(1,4) * .25;
+p = ones(1, 4) * .25;
 p(3) = -1 * .25;
 
 % subplot(2,2,2)
 bar(p, "yellow")
-set(gca,'XTick',[])
+set(gca, 'XTick', [])
 xlabel("States");
 ylabel("Probability amplitude")
-ylim([-1,1]); xlim([0,5])
+ylim([-1, 1]); xlim([0, 5])
 title("Altering the solution state to a negative sign")
 text(1, -0.1, '00');
 text(2, -0.1, '01');
@@ -37,29 +36,28 @@ text(3, -0.3, '10');
 text(4, -0.1, '11');
 
 hold on
-p = ones(1,6) * .25;
-plot(0:5,p, Color="r", LineStyle="--", LineWidth=1)
-
+p = ones(1, 6) * .25;
+plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
 
 %%
 
 clear;
-p = ones(1,4) * .25;
+p = ones(1, 4) * .25;
 p(3) = 3 * .25;
 
 % subplot(2,2,[3,4])
 bar(p, "yellow")
-set(gca,'XTick',[])
+set(gca, 'XTick', [])
 xlabel("States");
 ylabel("Probability amplitude")
-ylim([-1,1]); xlim([0,5])
-title("Inversion about the mean with amplication")
+ylim([-1, 1]); xlim([0, 5])
+title("Inversion about the mean with amplification")
 text(1, -0.1, '00');
 text(2, -0.1, '01');
 text(3, -0.1, '10');
 text(4, -0.1, '11');
 
 hold on
-p = ones(1,6) * .25;
-plot(0:5,p, Color="r", LineStyle="--", LineWidth=1)
-saveas(gcf,"fig3.png")
+p = ones(1, 6) * .25;
+plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
+saveas(gcf, "fig3.png")

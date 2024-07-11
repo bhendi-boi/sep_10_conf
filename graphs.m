3d5dc5c47c19e76c6223e45d785b19e4a3a400b1
bhendi-boi <bhendiisgreen@gmail.com>
2024-07-11 10:12:38 +0530

    updated fig3.png

--- graphs.m
+++ graphs.m
@@ -1,43 +1,43 @@
-clear; close all
-p = ones(1, 4) * .25;
+% clear; close all
+% p = ones(1, 4) * .25;
 
-% subplot(2,2,1)
-bar(p, "yellow");
-set(gca, 'XTick', [])
-xlabel("States");
-ylabel("Probability amplitude")
-ylim([-1, 1]); xlim([0, 5])
-title("Two qubits in a state of superposition")
-text(1, -0.1, '00');
-text(2, -0.1, '01');
-text(3, -0.1, '10');
-text(4, -0.1, '11');
+% % subplot(2,2,1)
+% bar(p, "yellow");
+% set(gca, 'XTick', [])
+% xlabel("States");
+% ylabel("Probability amplitude")
+% ylim([-1, 1]); xlim([0, 5])
+% title("Two qubits in a state of superposition")
+% text(1, -0.1, '00');
+% text(2, -0.1, '01');
+% text(3, -0.1, '10');
+% text(4, -0.1, '11');
 
-hold on
-p = ones(1, 6) * .25;
-plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
+% hold on
+% p = ones(1, 6) * .25;
+% plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
 
-%%
+% %%
 
-clear;
-p = ones(1, 4) * .25;
-p(3) = -1 * .25;
+% clear;
+% p = ones(1, 4) * .25;
+% p(3) = -1 * .25;
 
-% subplot(2,2,2)
-bar(p, "yellow")
-set(gca, 'XTick', [])
-xlabel("States");
-ylabel("Probability amplitude")
-ylim([-1, 1]); xlim([0, 5])
-title("Altering the solution state to a negative sign")
-text(1, -0.1, '00');
-text(2, -0.1, '01');
-text(3, -0.3, '10');
-text(4, -0.1, '11');
+% % subplot(2,2,2)
+% bar(p, "yellow")
+% set(gca, 'XTick', [])
+% xlabel("States");
+% ylabel("Probability amplitude")
+% ylim([-1, 1]); xlim([0, 5])
+% title("Altering the solution state to a negative sign")
+% text(1, -0.1, '00');
+% text(2, -0.1, '01');
+% text(3, -0.3, '10');
+% text(4, -0.1, '11');
 
-hold on
-p = ones(1, 6) * .25;
-plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
+% hold on
+% p = ones(1, 6) * .25;
+% plot(0:5, p, Color = "r", LineStyle = "--", LineWidth = 1)
 
 %%
 
